b88abd59f95d2c2dbb2387e3f03fa8bd56c94547
felicity <reddel.feli@googlemail.com>
2022-05-22 10:03:50 +0200

    open exploration results (indiv-max)

--- perform_experiments.py
+++ perform_experiments.py
@@ -112,55 +112,55 @@ def get_levers():
 
 if __name__ == "__main__":
 
-    # all-off & all-max
-    policies = [
-        Policy('all off', **{'mlit_select': 0,
-                             'del_t': 0,
-                             'rank_punish': 0,
-                             'rank_t': 0,
-                             'strikes_t': 0}),
-        Policy('all max', **{'mlit_select': 4,
-                             'del_t': 2,
-                             'rank_punish': 4,
-                             'rank_t': 2,
-                             'strikes_t': 2}),
-    ]
-
-    # # indiv-max
+    # # all-off & all-max
     # policies = [
-    #     Policy('mlit_select max', **{'mlit_select': 4,
-    #                                  'del_t': 0,
-    #                                  'rank_punish': 0,
-    #                                  'rank_t': 0,
-    #                                  'strikes_t': 0}),
-    #     Policy('del_t max', **{'mlit_select': 0,
-    #                            'del_t': 2,
-    #                            'rank_punish': 0,
-    #                            'rank_t': 0,
-    #                            'strikes_t': 0}),
-    #     Policy('rank_punish max', **{'mlit_select': 0,
-    #                                  'del_t': 0,
-    #                                  'rank_punish': 4,
-    #                                  'rank_t': 0,
-    #                                  'strikes_t': 0}),
-    #     Policy('rank_t max', **{'mlit_select': 0,
-    #                             'del_t': 0,
-    #                             'rank_punish': 0,
-    #                             'rank_t': 2,
-    #                             'strikes_t': 0}),
-    #     Policy('strikes_t max', **{'mlit_select': 0,
-    #                                'del_t': 0,
-    #                                'rank_punish': 0,
-    #                                'rank_t': 0,
-    #                                'strikes_t': 2}),
+    #     Policy('all off', **{'mlit_select': 0,
+    #                          'del_t': 0,
+    #                          'rank_punish': 0,
+    #                          'rank_t': 0,
+    #                          'strikes_t': 0}),
+    #     Policy('all max', **{'mlit_select': 4,
+    #                          'del_t': 2,
+    #                          'rank_punish': 4,
+    #                          'rank_t': 2,
+    #                          'strikes_t': 2}),
     # ]
+
+    # indiv-max
+    policies = [
+        Policy('mlit_select max', **{'mlit_select': 4,
+                                     'del_t': 0,
+                                     'rank_punish': 0,
+                                     'rank_t': 0,
+                                     'strikes_t': 0}),
+        Policy('del_t max', **{'mlit_select': 0,
+                               'del_t': 2,
+                               'rank_punish': 0,
+                               'rank_t': 0,
+                               'strikes_t': 0}),
+        Policy('rank_punish max', **{'mlit_select': 0,
+                                     'del_t': 0,
+                                     'rank_punish': 4,
+                                     'rank_t': 0,
+                                     'strikes_t': 0}),
+        Policy('rank_t max', **{'mlit_select': 0,
+                                'del_t': 0,
+                                'rank_punish': 0,
+                                'rank_t': 2,
+                                'strikes_t': 0}),
+        Policy('strikes_t max', **{'mlit_select': 0,
+                                   'del_t': 0,
+                                   'rank_punish': 0,
+                                   'rank_t': 0,
+                                   'strikes_t': 2}),
+    ]
 
     # res = perform_my_experiments(policies=policy_list, scenarios=10, saving=True)
     # policies = 50
     scenarios = 50
     steps = 60
-    exp_name = "all_off_all_max"
-    # exp_name = "indiv_max"
+    # exp_name = "all_off_all_max"
+    exp_name = "indiv_max"
 
     beliefs = list(BeliefUpdate)
 
