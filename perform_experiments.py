bfc2ccf85273e24b24540cac8008ed374801e170
felicity <reddel.feli@googlemail.com>
2022-05-22 14:22:32 +0200

    Feature Scoring Setup, project hygiene (e.g., utils_dmdu.py)

--- perform_experiments.py
+++ perform_experiments.py
@@ -14,6 +14,14 @@ from ema_workbench import (
 from ema_workbench.em_framework.parameters import Category
 from misinfo_model import MisinfoPy
 from enums import BeliefUpdate
+from utils_dmdu import (
+    get_constants,
+    get_uncertainties,
+    get_levers,
+    get_outcomes,
+    get_policies_all,
+    get_policies_indiv
+)
 
 ema_logging.log_to_stderr(ema_logging.INFO)
 
@@ -35,15 +43,8 @@ def perform_my_experiments(policies, scenarios, belief_update_fn, steps=60, savi
     model = MisinfoPy()
     model = Model('MisinfoPy', function=model)
 
-    model.uncertainties = [IntegerParameter('belief_metric_threshold', 75, 80),
-                           IntegerParameter('n_edges', 2, 3),
-                           RealParameter('ratio_normal_user', 0.98, 0.995),  # other parts need this to stay RealParam
-                           IntegerParameter('mean_normal_user', 0, 2),
-                           IntegerParameter('mean_disinformer', 8, 12),
-                           RealParameter('high_media_lit', 0.25, 0.35)]   # other parts need this to stay RealParam
-
-    model.constants = [Constant('steps', steps),
-                       Constant('belief_update_fn', belief_update_fn)]
+    model.uncertainties = get_uncertainties()
+    model.constants = get_constants()
     model.outcomes = get_outcomes()
     model.levers = get_levers()
 
@@ -57,105 +58,6 @@ def perform_my_experiments(policies, scenarios, belief_update_fn, steps=60, savi
         save_results(results, file_name)
 
     return results
-
-
-# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-# Helper funs
-# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-
-
-def get_outcomes():
-    """
-    Returns the outcomes. In the fitting format for the ema_workbench.
-    @return:
-    """
-    outcomes = [
-        ScalarOutcome('n_agents_above_belief_threshold', ScalarOutcome.MAXIMIZE),
-        ScalarOutcome('polarization_variance', ScalarOutcome.MINIMIZE),
-        ScalarOutcome('engagement', ScalarOutcome.MAXIMIZE),
-        ScalarOutcome('free_speech_constraint', ScalarOutcome.MINIMIZE),
-        ScalarOutcome('avg_user_effort', ScalarOutcome.MINIMIZE)
-    ]
-
-    return outcomes
-
-
-def get_levers():
-    """
-    Returns the levers. In the fitting format for the ema_workbench.
-    @return: list of CategoricalParameter
-    """
-
-    levers = [
-        IntegerParameter('mlit_select', lower_bound=0, upper_bound=10),
-        IntegerParameter('del_t', lower_bound=0, upper_bound=5),
-        IntegerParameter('rank_punish', lower_bound=0, upper_bound=10),
-        IntegerParameter('rank_t', lower_bound=0, upper_bound=5),
-        IntegerParameter('strikes_t', lower_bound=0, upper_bound=5),
-    ]
-
-    return levers
-
-
-def get_policies_all():
-    """Returns list of 2 Policy objects: all-off and all-max."""
-
-    policy_list = [
-        Policy('all off', **{'mlit_select': 0,
-                             'del_t': 0,
-                             'rank_punish': 0,
-                             'rank_t': 0,
-                             'strikes_t': 0}),
-        Policy('all max', **{'mlit_select': 10,
-                             'del_t': 5,
-                             'rank_punish': 10,
-                             'rank_t': 5,
-                             'strikes_t': 5}),
-    ]
-
-    return policy_list
-
-
-def get_policies_indiv():
-    """
-    Returns list of 5 Policy objects:
-    Each individual lever maximized while the other levers are on a very low value.
-    """
-
-    policy_list = [
-        Policy('mlit_select max', **{'mlit_select': 10,
-                                     'del_t': 0,
-                                     'rank_punish': 0,
-                                     'rank_t': 0,
-                                     'strikes_t': 0}),
-        Policy('del_t max', **{'mlit_select': 0,
-                               'del_t': 5,
-                               'rank_punish': 0,
-                               'rank_t': 0,
-                               'strikes_t': 0}),
-        Policy('rank_punish max', **{'mlit_select': 0,
-                                     'del_t': 0,
-                                     'rank_punish': 10,
-                                     'rank_t': 1,
-                                     'strikes_t': 0}),
-        Policy('rank_t max', **{'mlit_select': 0,
-                                'del_t': 0,
-                                'rank_punish': 1,
-                                'rank_t': 5,
-                                'strikes_t': 0}),
-        Policy('double rank max', **{'mlit_select': 0,
-                                     'del_t': 0,
-                                     'rank_punish': 10,
-                                     'rank_t': 5,
-                                     'strikes_t': 0}),
-        Policy('strikes_t max', **{'mlit_select': 0,
-                                   'del_t': 0,
-                                   'rank_punish': 0,
-                                   'rank_t': 0,
-                                   'strikes_t': 5})
-    ]
-
-    return policy_list
 
 
 if __name__ == "__main__":
